291dbdeb34713f82c5f08f514d64800f9bd986c3
Robert Corrado <82347372+robbycorrs23@users.noreply.github.com>
2021-11-23 23:06:14 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,9 +27,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          Powered by Big Daddy Plumbing!!
           </span>
         </a>
       </footer>
